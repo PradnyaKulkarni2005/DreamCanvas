9a279c7dc6803b4263285b8cd10cbe3d91a8f53a
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2025-07-08 07:50:40 +0530

    build error fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ export default function NotFound() {
       <h1 className="text-5xl font-extrabold mb-6 text-red-500">404</h1>
       <h2 className="text-3xl font-semibold mb-2">Page Not Found</h2>
       <p className="text-lg text-gray-300 mb-6">
-        Sorry, the page you are looking for doesn&apos't exist or has been moved.
+        Sorry, the page you are looking for doesn&apost exist or has been moved.
       </p>
         <Link href="/" className="px-6 py-2 bg-red-500 hover:bg-red-600 text-white font-semibold rounded-lg transition">
             Go to Home
