18c5b99653ba316972fa94cf54ca1ec723f95c3c
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2025-07-08 07:49:06 +0530

    build error fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 // This component is rendered when a page is not found (404 error).
 // NotFound is a special component in Next.js that is used to handle 404 errors. and it is automatically used when a page is not found.It doesn't accept any props or parameters.
 export default function NotFound() {
@@ -7,14 +8,12 @@ export default function NotFound() {
       <h1 className="text-5xl font-extrabold mb-6 text-red-500">404</h1>
       <h2 className="text-3xl font-semibold mb-2">Page Not Found</h2>
       <p className="text-lg text-gray-300 mb-6">
-        Sorry, the page you are looking for doesn't exist or has been moved.
+        Sorry, the page you are looking for doesn&apos't exist or has been moved.
       </p>
-      <a
-        href="/"
-        className="px-6 py-2 bg-red-500 hover:bg-red-600 text-white font-semibold rounded-lg transition"
-      >
-        Go Back Home
-      </a>
+        <Link href="/" className="px-6 py-2 bg-red-500 hover:bg-red-600 text-white font-semibold rounded-lg transition">
+            Go to Home
+        </Link>
+      
     </div>
   );
 }
